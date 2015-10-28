e494dea3d6133d4fc063f677ba59b312324f754e
Werror23 <vk.spacer23@gmail.com>
2015-10-28 21:03:47 +0300

    +multiplication; struct -> class; +constr; + destr;

--- main.cpp
+++ main.cpp
@@ -4,73 +4,114 @@
 
 using namespace std;
 
-struct matr_t
+class matr_t
 {
+public:
+    matr_t(int row_cnt, int col_cnt)
+    {
+        col_cnt_ = col_cnt;
+        row_cnt_ = row_cnt;
+
+        data_ = new int*[row_cnt];
+        for (int m = 0; m < col_cnt; ++m)
+            data_[m] = new int[col_cnt];
+    }
+    
+    ~matr_t()
+    {
+        for (int i = 0; i < row_cnt_; ++i)
+            delete[] data_[i];
+
+        delete[] data_;
+    }
+    
+    void fillrand_matr()
+    {
+        for (int i = 0; i < row_cnt_ ; i++)
+            for (int j = 0; j < col_cnt_ ; j++)
+                data_[i][j] = rand() % 10 + 1;
+       
+    }
+    void print_matr() const
+    {
+        for (int i = 0; i < row_cnt_; i++)
+        {
+            for (int j = 0; j < col_cnt_; j++)
+                cout << setw(4) << data_[i][j];
+            cout << "\n";
+        }
+    }
+    
+    int get_col_cnt() const
+    {
+        return col_cnt_;
+    }
+    
+    int get_row_cnt() const
+    {
+        return row_cnt_;
+    }
+    
+    int get_data(int i, int j) const
+    {
+        return data_[i][j];
+    }
+    
+    void set_data(int i, int j, int value) 
+    {
+        data_[i][j] = value;
+    }
+    
+private:
     int col_cnt_;
     int row_cnt_;
     
     int ** data_;
 };
 
-void allocate_matr( matr_t * matr, int row_cnt, int col_cnt )
+void sum_matr( matr_t const * a, matr_t const * b, matr_t * c )
 {
-    matr->col_cnt_ = col_cnt;
-    matr->row_cnt_ = row_cnt;
-    
-    matr->data_ = new int*[row_cnt];
-    for (int m = 0; m < col_cnt; ++m)
-        matr->data_[m] = new int[col_cnt];
+    for (int i = 0; i < c->get_row_cnt() ; i++)
+        for (int j = 0; j < c->get_col_cnt() ; j++)
+            c->set_data(i, j, a->get_data(i, j) + b->get_data(i, j));
 }
 
-void fillrand_matr( matr_t *matr )
+void mul_matr( matr_t const *a, matr_t const *b, matr_t *c )
 {
-    for (int i = 0; i < matr->row_cnt_ ; i++)
-        for (int j = 0; j < matr->col_cnt_ ; j++)
-            matr->data_[i][j] = rand() % 10 + 1;
-       
-}
-
-void sum_matr( matr_t *a, matr_t *b, matr_t *c )
-{
-    for (int i = 0; i < c->row_cnt_ ; i++)
-        for (int j = 0; j < c->col_cnt_ ; j++)
-            c->data_[i][j] = a->data_[i][j] + b->data_[i][j];
-        
-}
-
-void print_matr( matr_t matr )
-{
-    for (int i = 0; i < matr.row_cnt_; i++)
-    {
-        for (int j = 0; j < matr.col_cnt_; j++)
-            cout << setw(3) << matr.data_[i][j];
-        cout << "\n";
-    }
-    
+    int v;
+    for (int i = 0; i < c->get_row_cnt() ; i++)
+        for (int j = 0; j < c->get_col_cnt() ; j++)
+        {
+            v = 0;
+            for (int m = 0; m < c->get_row_cnt(); m++)
+                v += a->get_data(i, m) * b->get_data(m, j);
+            c->set_data(i, j, v);
+        }
 }
 
 int main(int argc, char** argv)
 {
     int col_cnt = 3, row_cnt = 3;    
     
-    matr_t a, b, c;
+    matr_t   a(row_cnt, col_cnt)
+           , b(row_cnt, col_cnt)
+           , c(row_cnt, col_cnt)
+           , d(row_cnt, col_cnt);
     
-    a.data_ = b.data_ = c.data_ = NULL;
-    
-    allocate_matr(&a, row_cnt, col_cnt);
-    allocate_matr(&b, row_cnt, col_cnt);
-    allocate_matr(&c, row_cnt, col_cnt);
-
-    fillrand_matr(&a);
-    fillrand_matr(&b);
+    a.fillrand_matr();
+    b.fillrand_matr();
     sum_matr(&a, &b, &c);
+    mul_matr(&a, &b, &d);
+    
     
     cout << " a:\n";
-    print_matr(a);
+    a.print_matr();
     cout << "\n b:\n";
-    print_matr(b);
-    cout << "\n c:\n";
-    print_matr(c);
+    b.print_matr();
+    cout << "\n Sum:\n";
+    c.print_matr();
+    cout << "\n Mul:\n";
+    d.print_matr();
     
     return 0;
 }
